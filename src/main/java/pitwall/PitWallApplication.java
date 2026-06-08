542a6d3858f0468e0ad4012b5f06767573fdcd56
Ali_Umar_Babar <ali.babar@ulb.be>
2026-06-08 23:03:32 +0200

    initialisation of the spring-boot project

---
+++ src/main/java/pitwall/PitWallApplication.java
@@ -0,0 +1,13 @@
+package pitwall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitWallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PitWallApplication.class, args);
+    }
+        
+}
---
+++ src/test/java/pitwall/PitWallApplicationTests.java
@@ -0,0 +1,12 @@
+package pitwall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitWallApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
